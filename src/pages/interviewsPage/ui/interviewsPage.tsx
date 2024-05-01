c59257a5b2a7121ae24f178fdf149894d4426dec
Gritzaev Nikita <gritzaevnikita@gmail.com>
2024-05-01 16:32:10 +0700

    Add navbar and content styles

---
+++ src/pages/interviewsPage/ui/interviewsPage.tsx
@@ -0,0 +1,3 @@
+export const InterviewsPage = () => {
+  return 'Страница собеседований'
+}
--- src/shared/config/routes.ts
+++ src/shared/config/routes.ts
@@ -2,4 +2,7 @@ export enum AppRoutes {
   MAIN = '/',
   LOGIN = '/login',
   REGISTER = '/register',
+  PROFILE = '/profile',
+  INTERVIEWS = '/interviews',
+  PRACTICE = '/practice',
 }
