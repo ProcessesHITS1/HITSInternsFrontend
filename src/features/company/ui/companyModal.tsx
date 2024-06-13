35e48449163e81b8e30e0d9fe304c95933ef60af
Gritzaev Nikita <gritzaevnikita@gmail.com>
2024-06-13 11:13:45 +0700

    Fix

--- src/features/company/ui/companyModal.tsx
+++ src/features/company/ui/companyModal.tsx
@@ -82,6 +82,7 @@ export const CompanyModal = (props: CompanyModalProps) => {
                 {
                   required: true,
                   message: 'Введите название',
+                  whitespace: true,
                 },
               ]}
               label='Название'
