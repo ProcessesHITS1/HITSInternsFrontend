db6e69b35e3325d88bddcff70840293f80823e0a
Gritzaev Nikita <gritzaevnikita@gmail.com>
2024-06-27 16:32:32 +0700

    add season sort

--- src/pages/practicePage/ui/practicePage.tsx
+++ src/pages/practicePage/ui/practicePage.tsx
@@ -1,4 +1,4 @@
-import { Spin, Typography } from 'antd'
+import { Select, Spin, Typography } from 'antd'
 import { useState } from 'react'
 import { SemesterCloneModal, SemesterModal } from '~features/semester'
 import { useGetAllSeasonsQuery } from '~entities/season'
@@ -6,6 +6,7 @@ import { Semester, SemestersList, useGetAllSemestersQuery } from '~entities/seme
 
 export const PracticePage = () => {
   const semestersQuery = useGetAllSemestersQuery({ page: 1, size: 100000 })
+  const [select, setSelect] = useState(null as string | null)
   const seasonsQuery = useGetAllSeasonsQuery()
   const [modalState, setModalState] = useState({
     open: false,
@@ -25,6 +26,8 @@ export const PracticePage = () => {
     return <Spin size='large' className='mt-5' />
   }
 
+  const hasSeasons = seasonsQuery.data?.length || 0 > 0
+
   return (
     <>
       <SemesterModal
@@ -39,11 +42,28 @@ export const PracticePage = () => {
         close={() => setCloneModalState({ ...cloneModalState, open: false })}
       />
       <Typography.Title level={4}>Семестры практики</Typography.Title>
+      {hasSeasons && (
+        <Select
+          value={select}
+          onChange={setSelect}
+          allowClear
+          size='small'
+          placeholder='Сезон'
+          className='w-[144px] mt-1'
+          options={
+            seasonsQuery.data
+              ?.toSorted((a, b) => b.year - a.year)
+              .map((s) => ({ label: `Сезон-${s.year}`, value: s.id })) || []
+          }
+        />
+      )}
       <SemestersList
         semesters={
-          semestersQuery.data?.data.toSorted(
-            (a, b) => b.documentsDeadline?.localeCompare(a.documentsDeadline || '') || 0
-          ) || []
+          semestersQuery.data?.data
+            .toSorted(
+              (a, b) => b.documentsDeadline?.localeCompare(a.documentsDeadline || '') || 0
+            )
+            .filter((s) => !select || s.seasonId === select) || []
         }
         seasons={seasonsQuery.data || []}
         openEditModal={(semester) => setModalState({ semester, open: true })}
